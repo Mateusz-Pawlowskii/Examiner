04e1d37c04d0e3c5cff5d4aeaac71615097fefae
Mateusz Pawlowski <aleksythepony@gmail.com>
2022-10-29 13:50:23 +0200

    diploma fix

--- student/views.py
+++ student/views.py
@@ -197,7 +197,7 @@ class TestDiploma(LoginRequiredMixin, View):
         pdf.cell(0, 50, txt = "", ln = 1, align = 'C')
         pdf.cell(0, 5, txt = f"Dyplom zaliczenia kursu {course.name}", ln = 1, align = 'C')
         pdf.cell(0, 10, txt = f"z wynikiem {best_result.current_score} na {course.question_amount} przez studenta {user.username}", ln = 1, align = 'C')
-        pdf.cell(0, 50, txt = "Gratulacje", ln = 1, align = 'C')
+        pdf.cell(0, 50, txt = "Gratulacje", ln = 1, align = 'C') 
         pdf.output(f"media/diploma/{course.name}_{user.username}.pdf")
         return FileResponse(open(f"media/diploma/{course.name}_{user.username}.pdf", "rb"), content_type="application/pdf")
     
