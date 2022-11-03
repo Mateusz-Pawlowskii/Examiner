7dd70bb493f6c4a2e226608d509f20f0d15054ef
Mateusz Pawlowski <aleksythepony@gmail.com>
2022-11-03 21:14:45 +0100

    search course fix

--- static/js/course_search.js
+++ static/js/course_search.js
@@ -1,12 +1,10 @@
-function search1 () {
+function search1 (event) {
     document.getElementById("form1").style.display = "block";
     document.getElementById("form2").style.display = "none";
-    console.log("search1")
 }
-function search2 () {
+function search2 (event) {
     document.getElementById("form1").style.display = "none";
     document.getElementById("form2").style.display = "block";
-    console.log("search2")
 }
 function categorySearch () {
     const content = document.getElementById("id_category");
