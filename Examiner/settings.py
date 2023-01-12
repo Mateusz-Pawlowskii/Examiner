60c3f1dff5a3e7d1fd2f2f9f4fda77c0fd17cc5c
Mateusz Pawlowski <aleksythepony@gmail.com>
2023-01-12 20:16:00 +0100

    staticfiles changed to local

--- Examiner/settings.py
+++ Examiner/settings.py
@@ -174,7 +174,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
 import django_on_heroku
-django_on_heroku.settings(locals(), staticfiles=False)
+django_on_heroku.settings(locals())
 
 # mail config
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
