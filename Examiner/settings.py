c7c83625fc3c273b393ecbc93bb2582178f32b25
Mateusz Pawlowski <aleksythepony@gmail.com>
2023-01-12 19:44:17 +0100

    settings fix

--- Examiner/settings.py
+++ Examiner/settings.py
@@ -160,13 +160,13 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = f'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_URL = f'/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '/static/'),)
 # PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = f"media/"
-MEDIAFILES_DIRS = (os.path.join(BASE_DIR, 'media/'),)
+MEDIA_URL = f"/media/"
+MEDIAFILES_DIRS = (os.path.join(BASE_DIR, '/media/'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
