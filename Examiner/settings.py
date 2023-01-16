9ad831a99fda3b5e90b376b1a1bdf479fb5b7f16
Mateusz Pawlowski <aleksythepony@gmail.com>
2023-01-16 20:34:28 +0100

    settings cleanup

--- Examiner/settings.py
+++ Examiner/settings.py
@@ -163,7 +163,7 @@ AWS_HEADERS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = f'static/'
+# STATIC_URL = f'static/' 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 # MEDIA_URL = f"media/"
