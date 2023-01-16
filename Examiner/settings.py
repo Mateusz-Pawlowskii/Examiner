a4f181dd6250dfdd4e2c060035efa9ff38708ce6
Mateusz Pawlowski <aleksythepony@gmail.com>
2023-01-16 17:48:09 +0100

    AWS_LOCATION disabled

--- Examiner/settings.py
+++ Examiner/settings.py
@@ -150,7 +150,7 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl" : "max-age=86400"
 }
-AWS_LOCATION = 'static'
+# AWS_LOCATION = 'static'
 AWS_QUERYSTRING_AUTH = False
 AWS_HEADERS = {
     "Access-Control-Allow-Origin" : "*"
