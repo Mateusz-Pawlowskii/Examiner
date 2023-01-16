e08c621e113eb572d97ccf7c1307df95896c7210
Mateusz Pawlowski <aleksythepony@gmail.com>
2023-01-16 20:37:56 +0100

    settings test change

--- Examiner/settings.py
+++ Examiner/settings.py
@@ -163,11 +163,11 @@ AWS_HEADERS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = f'static/' 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-# STATIC_ROOT = BASE_DIR / "staticfiles"
-# MEDIA_URL = f"media/"
-# MEDIA_ROOT = BASE_DIR / "media"
+STATIC_URL = f'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_URL = f"media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
