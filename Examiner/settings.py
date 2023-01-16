00f6be0c1c8fc3afe363c3000b4dcaaa30cc592b
Mateusz Pawlowski <aleksythepony@gmail.com>
2023-01-16 16:15:45 +0100

    removed MEDIAFILES_DIR constant

--- Examiner/settings.py
+++ Examiner/settings.py
@@ -167,7 +167,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = f"media/"
-MEDIAFILES_DIRS = (os.path.join(BASE_DIR, 'media/'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
