8286f8b282bea6c317a59db18b1b33c567cc5f23
Mateusz Pawlowski <aleksythepony@gmail.com>
2023-01-16 20:44:37 +0100

    STATICFILES_DIRS update

--- Examiner/settings.py
+++ Examiner/settings.py
@@ -164,7 +164,7 @@ AWS_HEADERS = {
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = f'static/'
-STATICFILES_DIRS = BASE_DIR / 'static'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'))
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = f"media/"
 MEDIA_ROOT = BASE_DIR / "media"
