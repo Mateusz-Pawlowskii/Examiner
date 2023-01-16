15f0856d2e01b372c491a5dd7c5b1fcfe3d68b9e
Mateusz Pawlowski <aleksythepony@gmail.com>
2023-01-16 20:21:31 +0100

    MEDIA_ROOT update

--- Examiner/settings.py
+++ Examiner/settings.py
@@ -167,7 +167,7 @@ STATIC_URL = f'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = f"media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com' / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
